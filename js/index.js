cc39263b577bf0a52aa5b9c19fe0d5436e02b052
Nadar <nadarhilcuk@gmail.com>
2024-06-10 19:01:48 +0200

    add new calculator apple

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const applePrice = 10; 
+
+const appleQuantity = 5;
+
+const totalCost = applePrice * appleQuantity;
+
+const message = `Загальна вартість ${appleQuantity} яблук становить ${totalCost} гривень.`;
+
+console.log(message);
+
+console.log(`Тип змінної applePrice: ${typeof applePrice}`);
+console.log(`Тип змінної appleQuantity: ${typeof appleQuantity}`);
+console.log(`Тип змінної message: ${typeof message}`);
